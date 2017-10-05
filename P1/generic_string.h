cd482760e35d838952c945caf837a08139e5b10e
Carlos Li Hu <carloslihu96@gmail.com>
2017-10-05 19:48:51 +0200

    main preparado

--- P1/generic_string.h
+++ P1/generic_string.h
@@ -1,4 +1,9 @@
 #ifndef GENERIC_STRING_H
 #define GENERIC_STRING_H
+#include "basic_types.h"
+
+void destroy_p_string(void* p_string);
+void* copy_p_string(const void* p_string);
+int print_p_string(FILE * fd, const void* p_string);
 
 #endif
