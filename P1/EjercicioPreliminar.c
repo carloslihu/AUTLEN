a713d335435e99f040d4aaf92f4fa2a497b8bce1
Carlos Li Hu <carloslihu96@gmail.com>
2017-10-05 18:45:41 +0200

    preliminar hecho

--- P1/EjercicioPreliminar.c
+++ P1/EjercicioPreliminar.c
@@ -22,7 +22,12 @@ void print_double(FILE * fd, void * p_double)
 
 }
 
+void print_string(FILE * fd, void * p_string)
+{
+	fprintf(fd, "(STRING) %s \n", (char*)p_string);
 
+
+}
 
 
 
@@ -46,16 +51,17 @@ int main(int argc, char ** argv)
 	c2.print_elemento = print_double;
 
 
-	/*
-	c3.elemento = ;
-	c3.print_elemento = ;
-	*/
+	
+	c3.elemento = texto;
+	c3.print_elemento = print_string;
+	
 
 	c1.print_elemento(stdout, c1.elemento);
 	c2.print_elemento(stdout, c2.elemento);
+	
 
 	/***Añade aquí el código necesario para imprimir una cadena de caracteres***/
-
+	c3.print_elemento(stdout, c3.elemento);
 
 	return 0;
 
