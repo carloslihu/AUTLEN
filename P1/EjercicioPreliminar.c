d8e620b59c845adb6fc3c80af2b39f8e82d78525
Carlos Li Hu <carloslihu96@gmail.com>
2017-10-05 18:30:27 +0200

    .c creado

--- P1/EjercicioPreliminar.c
+++ P1/EjercicioPreliminar.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-typedef struct _clase 
+typedef struct _clase
 {
 	void * elemento;
 	void (*print_elemento)(FILE *, void *);
@@ -9,7 +9,7 @@ typedef struct _clase
 
 void print_int(FILE * fd, void * p_int)
 {
-	fprintf(fd,"(INT) %d \n",*((int*)p_int));
+	fprintf(fd, "(INT) %d \n", *((int*)p_int));
 
 
 }
@@ -17,7 +17,7 @@ void print_int(FILE * fd, void * p_int)
 
 void print_double(FILE * fd, void * p_double)
 {
-	fprintf(fd,"(DOUBLE) %E \n",*((double*)p_double));
+	fprintf(fd, "(DOUBLE) %E \n", *((double*)p_double));
 
 
 }
@@ -39,22 +39,22 @@ int main(int argc, char ** argv)
 	Clase c3;
 
 	c1.elemento = &i;
-	c1.print_elemento = print_int;	
+	c1.print_elemento = print_int;
 
 
 	c2.elemento = &d;
-	c2.print_elemento = print_double;	
+	c2.print_elemento = print_double;
 
 
 	/*
 	c3.elemento = ;
-	c3.print_elemento = ;	
+	c3.print_elemento = ;
 	*/
 
-	c1.print_elemento(stdout,c1.elemento);
-	c2.print_elemento(stdout,c2.elemento);
+	c1.print_elemento(stdout, c1.elemento);
+	c2.print_elemento(stdout, c2.elemento);
 
-   /***Añade aquí el código necesario para imprimir una cadena de caracteres***/
+	/***Añade aquí el código necesario para imprimir una cadena de caracteres***/
 
 
 	return 0;
